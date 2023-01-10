370ae64c2c6ccff29368e167169887ece3a3db6d
Giordano Temple <giordanotemple@gmail.com>
2023-01-10 19:30:20 +0000

    findkeybyvalue

---
+++ findKeyByValue.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, value) {
+  const keys = Object.keys(obj);
+  let result = undefined;
+
+  keys.forEach(key => {
+    if (obj[key] === value) {
+      result = key;
+    }
+  });
+
+  return result;
+};
+
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Random Test"), undefined);
