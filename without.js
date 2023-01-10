8edfe9770c4f2fe75d4796eb9af93327ef972561
Giordano Temple <giordanotemple@gmail.com>
2023-01-10 01:04:22 +0000

    without logic complete ... remember to review tomorrow

--- without.js
+++ without.js
@@ -1,5 +1,42 @@
 let without = function(source, itemsToRemove) {
-  let result = source.filter(item => {
-    
-  })
+  let result = [];
+  let flag = true;
+
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        flag = false;
+      }
+    }
+    if (flag) {
+      result.push(source[i]);
+    }
+    flag = true;
+  }
+  return result;
 }
+
+const assertArraysEqual = function(actual, expected) {
+  let failure = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  let success = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  let result = true;
+
+  if (actual.length !== expected.length) {
+    result = false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      result = false;
+    }
+  }
+  result ? console.log(success) : console.log(failure);
+};
+
+assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ['1', '2']);
+
+
+const words = ["hello", "world", "lighthouse"];
+assertArraysEqual(without(words, ["lighthouse"]), ["hello", "world"]);
+console.log(words)
