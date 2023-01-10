d5074b9822d31eefd3faf851769bacdabff5ba5c
Giordano Temple <giordanotemple@gmail.com>
2023-01-10 01:54:41 +0000

    flatten complete

---
+++ flatten.js
@@ -0,0 +1,33 @@
+const assertArraysEqual = function(actual, expected) {
+  let failure = `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  let success = `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  let result = true;
+
+  if (actual.length !== expected.length) {
+    result = false;
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      result = false;
+    }
+  }
+  result ? console.log(success) : console.log(failure);
+};
+
+let flatten = function(arr) {
+  let flattened = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        flattened.push(arr[i][j]);
+      }
+    } else {
+      flattened.push(arr[i]);
+    }
+  }
+  return flattened;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, 2, [3, 4, 5], 5, [6]]), [1, 2, 3, 4, 5, 5, 6]);
