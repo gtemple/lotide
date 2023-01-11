1c17f890e7c840ad859c2107c963363b0a78b40e
Giordano Temple <giordanotemple@gmail.com>
2023-01-11 23:41:10 +0000

    find key was a doozy

---
+++ findKey.js
@@ -0,0 +1,49 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  for (let i in obj) {
+    if (callback(obj[i])) {
+      return i;
+    }
+  }
+}
+
+
+let results1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+assertEqual(results1, 'noma')
+
+let results2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars > 2)
+
+assertEqual(results2, 'Akaleri')
+
+let results3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: true },
+  "tango":     { stars: true},
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === true)
+
+assertEqual(results3, 'Akaleri')
