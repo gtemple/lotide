4d02de3d65a340b826913b2c485efa5d57c9173b
Giordano Temple <giordanotemple@gmail.com>
2023-01-16 13:45:17 +0000

    refactored assertEqual and Head for testing

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 4);
-assertEqual("Toronto", "Toronto");
-assertEqual("Toronto", "Charawna");
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 4);
+assertEqual("Toronto", "Toronto");
+assertEqual("Toronto", "Charawna");
